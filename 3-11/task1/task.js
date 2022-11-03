b717ae1177db127870170a7c39bdcad60f1bf8ed
Ahmad-Hamaideh <iahmed.hamaideh@gmail.com>
2022-11-03 15:37:07 +0200

    Update

---
+++ 3-11/task1/task.js
@@ -0,0 +1,40 @@
+var x=-55 ;
+
+
+if (x < 0) {
+    document.write("invalid")
+}
+
+else if( x<50  ) 
+{
+    document.write ("FAIL")
+}
+
+
+else if(x <= 59){
+    document.write("D")
+}
+
+
+else if(x<=69){
+    document.write("C")
+}
+
+
+else if( x<=79){
+    document.write("B")
+}
+
+
+else if( x<=89){
+    document.write("A")
+}
+
+
+else if( x<=100){
+    document.write("A+")
+}
+
+else {
+    document.write("invalid")
+}
---
+++ 3-11/task2/task2.js
@@ -0,0 +1,5 @@
+const x=3.14;
+
+
+var y= x + 2 ;
+document.write(y)
